ecd8047dde728faab3eb769f64d5a62758920823
Vikas Thoutam <vikas.thoutam@gmail.com>
2025-09-02 17:33:42 -0500

    feat: add eslint and tailwind to main scaffolding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/eslint'
+  ],
+  eslint: {
+    // additional options here
+  }
 })
