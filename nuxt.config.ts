d06872344a229ec4d499e7ae736d9156301dc195
Vikas Thoutam <vikas.thoutam@gmail.com>
2025-09-04 15:33:57 -0500

    chore: replace tailwind for vite with nuxt tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
   modules: [
-    '@nuxt/eslint'
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss'
   ],
   eslint: {
     // additional options here
