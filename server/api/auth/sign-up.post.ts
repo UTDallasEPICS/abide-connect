0aa34bb8b64a9396cd9e926b6d15ec428ba1c7e1
Aagam Shah <shahaagam04@gmail.com>
2025-10-17 21:51:17 -0500

    added login and signup api routes

---
+++ server/api/auth/sign-up.post.ts
@@ -0,0 +1,9 @@
+import {auth} from "~/../lib/auth"
+
+export default defineEventHandler(async (event) => {
+    const {email, password} = await readBody(event)
+    
+    await auth.api.signUpEmail({body: {name, email, password}})
+
+    return { success: true }
+})
