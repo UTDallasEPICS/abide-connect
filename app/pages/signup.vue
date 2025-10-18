3e67308eadf9445f680bd02ed60caef6c1f64806
Aagam Shah <shahaagam04@gmail.com>
2025-10-18 16:49:59 -0500

    feat: add login and signup pages with form handling and user language support

---
+++ app/pages/signup.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { FormSubmitEvent } from "@nuxt/ui";
+import { signUpFormSchema } from "#shared/types/signUpFormTypes";
+import { SignUpFormSchema } from "#shared/types/signUpFormTypes";
+
+const state = reactive<Partial<SignUpFormSchema>>({
+    name: undefined,
+    email: undefined,
+    password: undefined,
+    language: []
+})
+
+const isLoading = ref(false)
+
+async function onSubmit(event: FormSubmitEvent<SignUpFormSchema>) {
+    isLoading.value = true
+    try{
+        useFetch("/api/auth/login", {
+            method: 'POST',
+            body: {
+                name:state.name,
+                email:state.email,
+                password:state.password,
+                phone:state.password,
+                language:state.language
+            }
+        })
+    } finally {
+        isLoading.value = false
+    }
+}
+</script>
+
+<template>
+<div class="flex bg-[] h-screen"></div>
+</template>
