feb28ab870c0be16bc5d8031d3b06bfef9a1bb20
Aagam Shah <shahaagam04@gmail.com>
2025-10-21 18:40:22 -0500

    functional login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -5,45 +5,77 @@ import type { LoginFormSchema } from "#imports";
 
 const state = reactive<Partial<LoginFormSchema>>({
     email: undefined,
-    password: undefined
-})
+    password: undefined,
+});
 
-const isLoading = ref(false)
+const isLoading = ref(false);
 
 async function onSubmit(event: FormSubmitEvent<LoginFormSchema>) {
-    isLoading.value = true
+    isLoading.value = true;
     try {
-        useFetch("/api/auth/login", {
-            method: 'POST',
+        await $fetch("/api/auth/login", {
+            method: "POST",
             body: {
-                email: state.email,
-                password: state.password,
-            }
-        })
+                email: event.data.email,
+                password: event.data.password,
+            },
+        });
     } finally {
-        isLoading.value = false
+        isLoading.value = false;
     }
 }
 </script>
 
 <template>
-    <div class="sm:w-[375px] bg-[#F2EBE3] flex flex-col justify-center h-screen px-8">
+    <div
+        class="sm:w-[375px] bg-[#F2EBE3] flex flex-col justify-center h-screen px-8"
+    >
         <h1 class="text-black font-bold text-4xl pb-6">Login</h1>
-        <UForm v-bind:schema="loginFormSchema" v-bind:state="state" class="space-y-4 " @submit.prevent="onSubmit">
-            <UFormField class="text-black text-2xl font-bold" label="Email" name="email" required>
-                <UInput v-model="state.email" leading-icon="i-lucide-at-sign" color="secondary" variant="soft" size="xl"
-                    class="w-full" />
+        <UForm
+            v-bind:schema="loginFormSchema"
+            v-bind:state="state"
+            class="space-y-4"
+            @submit.prevent="onSubmit"
+        >
+            <UFormField
+                class="text-black text-2xl font-bold"
+                label="Email"
+                name="email"
+                required
+            >
+                <UInput
+                    v-model="state.email"
+                    leading-icon="i-lucide-at-sign"
+                    color="secondary"
+                    variant="soft"
+                    size="xl"
+                    class="w-full"
+                />
             </UFormField>
 
-            <UFormField class="text-black text-2xl font-bold" label="Password" name="password" size="xl">
-                <UInput v-model="state.password" type="password" class="w-full" variant="soft"/> 
+            <UFormField
+                class="text-black text-2xl font-bold"
+                label="Password"
+                name="password"
+                size="xl"
+            >
+                <UInput
+                    v-model="state.password"
+                    type="password"
+                    class="w-full"
+                    variant="soft"
+                />
             </UFormField>
 
-            <UButton type="submit" class="bg-[#a26b61] w-[25%] justify-center " size="xl" >
+            <UButton
+                type="submit"
+                class="bg-[#a26b61] w-[25%] justify-center"
+                size="xl"
+                :loading="isLoading"
+                :disabled="isLoading"
+            >
                 <span class="font-bold text-[#a26b61/50]">Login</span>
             </UButton>
-
         </UForm>
-
     </div>
 </template>
