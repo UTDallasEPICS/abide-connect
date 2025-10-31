bd0b386da8f93c8b4655c39374957fedec954c9e
Aagam Shah <shahaagam04@gmail.com>
2025-10-31 02:03:11 -0500

    Refactor: update login form structure and enhance authentication UI components

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { FormSubmitEvent } from "@nuxt/ui";
-import { loginFormSchema } from "#shared/types/authFormTypes";
+import { loginFormSchema, loginFormFields, providers } from "#shared/types/authFormTypes";
 import type { LoginFormSchema } from "#imports";
+
 
 const state = reactive<Partial<LoginFormSchema>>({
     email: undefined,
@@ -30,52 +31,26 @@ async function onSubmit(event: FormSubmitEvent<LoginFormSchema>) {
     <div
         class="sm:w-[375px] bg-[#F2EBE3] flex flex-col justify-center h-screen px-8"
     >
-        <h1 class="text-black font-bold text-4xl pb-6">Login</h1>
-        <UForm
-            v-bind:schema="loginFormSchema"
-            v-bind:state="state"
-            class="space-y-4"
-            @submit.prevent="onSubmit"
-        >
-            <UFormField
-                class="text-black text-2xl font-bold"
-                label="Email"
-                name="email"
-                required
-            >
-                <UInput
-                    v-model="state.email"
-                    leading-icon="i-lucide-at-sign"
-                    color="secondary"
-                    variant="soft"
-                    size="xl"
-                    class="w-full"
-                />
-            </UFormField>
-
-            <UFormField
-                class="text-black text-2xl font-bold"
-                label="Password"
-                name="password"
-                size="xl"
-            >
-                <UInput
-                    v-model="state.password"
-                    type="password"
-                    class="w-full"
-                    variant="soft"
-                />
-            </UFormField>
-
-            <UButton
-                type="submit"
-                class="bg-[#a26b61] w-[25%] justify-center"
-                size="xl"
-                :loading="isLoading"
-                :disabled="isLoading"
-            >
-                <span class="font-bold text-[#a26b61/50]">Login</span>
-            </UButton>
-        </UForm>
+    <UAuthForm
+        :schema="loginFormSchema"
+        :fields="loginFormFields"
+        :providers="providers"
+        title="Welcome back!"
+        icon="i-lucide-lock"
+        @submit="onSubmit"
+      >
+        <template #description>
+          Don't have an account? <ULink to="#" class="text-primary font-medium">Sign up</ULink>.
+        </template>
+        <template #password-hint>
+          <ULink to="#" class="text-primary font-medium" tabindex="-1">Forgot password?</ULink>
+        </template>
+        <template #validation>
+          <UAlert color="error" icon="i-lucide-info" title="Error signing in" />
+        </template>
+        <template #footer>
+          By signing in, you agree to our <ULink to="#" class="text-primary font-medium">Terms of Service</ULink>.
+        </template>
+      </UAuthForm>
     </div>
 </template>
--- shared/types/authFormTypes.ts
+++ shared/types/authFormTypes.ts
@@ -1,5 +1,6 @@
 import * as z from "zod"
 import { Language } from "@prisma/client"
+import type { AuthFormField } from "@nuxt/ui"
 
 export const signUpFormSchema = z.object({
     name: z.string().optional(),
@@ -15,5 +16,35 @@ export const loginFormSchema = z.object({
     email: z.email('Invalid email'),
     password: z.string('Password is incorrect').min(8, "Must be at least 8 characters")
 })
+
+export const loginFormFields: AuthFormField[] = [{
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    placeholder: 'Enter your email',
+    required: true
+  }, {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    placeholder: 'Enter your password',
+    required: true
+  }, {
+    name: 'remember',
+    label: 'Remember me',
+    type: 'checkbox'
+  }]
+
+ export const providers = [{
+    label: 'Google',
+    icon: 'i-simple-icons-google',
+    onClick: () => {}
+  }, {
+    label: 'GitHub',
+    icon: 'i-simple-icons-github',
+    onClick: () => {}
+  }]
+  
+
 
 export type LoginFormSchema = z.output<typeof loginFormSchema>
