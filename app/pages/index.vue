3aea866ad0029d875741b734f87a9d63deb7415b
Aagam Shah <shahaagam04@gmail.com>
2025-10-31 04:17:14 -0500

    Refactor: remove template section from index.vue to streamline component structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,9 +4,3 @@ onMounted(() => {
   router.push('/home')
 })
 </script>
-
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <h1 class="text-3xl font-bold text-blue-600">🚀 Nuxt is working!</h1>
-  </div>
-</template>
