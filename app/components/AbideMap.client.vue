75e39c416c2d653a64d8d17035e2a9d89a3ea27c
Vikas Thoutam <vikas.thoutam@gmail.com>
2025-11-04 17:06:08 -0600

    chore: push clientonly change to fully component

---
+++ app/components/AbideMap.client.vue
@@ -0,0 +1,22 @@
+<template>
+  <MglMap
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+    :attribution-control="false"
+    >
+    <!--<MglNavigationControl />-->
+    <MglMarker
+      :coordinates="[center[0], center[1]]"
+      color="#FF0000"
+      />
+  </MglMap>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  style: string;
+  center: [number, number];
+  zoom: number;
+}>();
+</script>
