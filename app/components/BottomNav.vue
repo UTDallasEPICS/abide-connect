83f9bad50e7ec97befc03161b5cefaa20828e2d4
Aagam Shah <shahaagam04@gmail.com>
2025-11-05 03:31:57 -0600

    Update BottomNav.vue

--- app/components/BottomNav.vue
+++ app/components/BottomNav.vue
@@ -12,23 +12,18 @@ const nav = ref([
     },
     {
         id: 3,
-        icon: "i-lucide-square-user-round",
-        to: "/auth/sign-up",
-    },
-    {
-        id: 4,
         icon: "i-lucide-hospital",
         to: "/mobileClinic",
     },
     {
-        id: 5,
-        icon: "i-lucide-hand-heart",
-        to: "/home",
+        id: 4,
+        icon: "i-lucide-square-user-round",
+        to: "/auth/sign-up",
     },
 ]);
 </script>
 <template>
-<UFooter class="fixed bottom-0 left-0 right-0 flex justify-around items-center h-12 z-100 bg-white">
+<UFooter class="fixed bottom-0 left-0 right-0 flex justify-around items-center h-12 z-100 border-t border-gray-200 bg-white">
     <template #default>
         <UButton v-for="navItem in nav"
                 :key="navItem.id"
@@ -36,7 +31,7 @@ const nav = ref([
                 :to="navItem.to"
                 color="neutral"
                 variant="ghost"
-                class="w-full h-full mx-7"
+                class="w-full h-full mx-7 md:mx-16"
                 />
     </template>
 </UFooter>
