b64e9cd139644885fdcbcc5a80caaa09b53426eb
Aagam Shah <shahaagam04@gmail.com>
2025-11-05 03:06:00 -0600

    Add secondary layout component with header and back button

---
+++ app/layouts/secondary.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-screen h-screen bg-gray-100 items-center">
+        <UHeader :ui="{ toggle: 'hidden' }" class="fixed top-0 z-50 h-12 w-full bg-white backdrop-blur ">
+            <!-- Left Settings -->
+            <template #left>
+                <div class="">
+                    <UButton color="neutral" variant="ghost" aria-label="Back">
+                        <UIcon name="i-lucide-arrow-left-to-line" class="w-7 h-7" />
+                    </UButton>
+                </div>
+            </template>
+        </UHeader>
+        <slot />
+    </div>
+</template>
