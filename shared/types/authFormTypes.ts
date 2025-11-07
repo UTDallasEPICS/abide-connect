bf05da00dcade11be1958fbb39cd99554dd6582f
Aagam Shah <shahaagam04@gmail.com>
2025-11-06 22:23:52 -0600

    Refactor: update sign-up form fields to include phone, languages, gender, ethnicity, and availability

--- shared/types/authFormTypes.ts
+++ shared/types/authFormTypes.ts
@@ -1,21 +1,47 @@
 import * as z from "zod"
-import { Language } from "@prisma/client"
+import { Language, Ethinicity, Gender, Availability } from "@prisma/client"
+import type { InputMenuItem } from "@nuxt/ui"
 import type { AuthFormField } from "@nuxt/ui"
 
 export const signUpFormSchema = z.object({
     name: z.string().optional(),
-    email: z.email('Invalid email'),
+    email: z.string().email('Invalid email'),
     password: z.string().min(8, "Must be at least 8 characters"),
-    phone: z.e164().optional(),
-    language: z.enum(Language).array()
+    phone: z.string().regex(/^\+1\d{10}$/, "Invalid US phone number").optional(),
+    languages: z.array(z.enum(Language)),
+    gender: z.enum(Gender),
+    ethinicity: z.enum(Ethinicity),
+    availability: z.enum(Availability)
 })
 
 export type SignUpFormSchema = z.output<typeof signUpFormSchema>
 
 export const loginFormSchema = z.object({
     email: z.email('Invalid email'),
-    password: z.string('Password is incorrect').min(8, "Must be at least 8 characters")
+    password: z.string().min(8, "Must be at least 8 characters")
 })
+
+const languageItems: InputMenuItem[] = Object.keys(Language).map((language) => ({
+  id: language,
+  label:
+      language.toString().charAt(0).toUpperCase() +
+      language.toString().slice(1).toLowerCase(),
+}))
+const genderItems: InputMenuItem[] = Object.keys(Gender).map((gender) => ({
+  id: gender,
+  label:
+      gender.toString().charAt(0).toUpperCase() + gender.toString().slice(1).toLowerCase()
+}))
+const ethinicityItems: InputMenuItem[] = Object.keys(Ethinicity).map((ethinicity) => ({
+  id: ethinicity,
+  label:
+      ethinicity.toString().charAt(0).toUpperCase() + ethinicity.toString().slice(1).toLowerCase()
+}))
+const availabilityItems: InputMenuItem[] = Object.keys(Availability).map((availability) => ({
+  id: availability,
+  label:
+      availability.toString().charAt(0).toUpperCase() + availability.toString().slice(1).toLowerCase()
+}))
 
 export const loginFormFields: AuthFormField[] = [{
     name: 'email',
@@ -35,16 +61,65 @@ export const loginFormFields: AuthFormField[] = [{
     type: 'checkbox'
   }]
 
+export const signUpFormFields: AuthFormField[] = [{
+  name: 'email',
+  type: 'email',
+  label: 'Email',
+  placeholder: 'Enter your email',
+  required: true
+}, {
+  name: 'password',
+  label: 'Password',
+  type: 'password',
+  placeholder: 'Enter your password',
+  required: true
+},{
+  name: 'phone',
+  label: 'Phone',
+  type: 'text',
+  placeholder: 'Enter your US phone number',
+  required: false
+},{
+  name: 'languages',
+  label: 'Languages',
+  type: 'select',
+  placeholder: 'What languages do you speak?',
+  required: true,
+  items: languageItems,
+  multiple: true
+}, {
+  name: "gender",
+  label: 'Gender',
+  type: 'select',
+  placeholder: "What is your gender?",
+  items: genderItems,
+  required: true
+},{
+  name: "ethinicity",
+  label: 'Ethinicity',
+  type: 'select',
+  placeholder: "What is your ethinicity?",
+  items: ethinicityItems,
+  required: true
+},{
+  name: "availability",
+  label: 'Availability',
+  type: 'select',
+  placeholder: "What is your availability?",
+  items: availabilityItems,
+  required: true,
+  multiple: true
+},]
+
  export const providers = [{
     label: 'Google',
     icon: 'i-simple-icons-google',
     onClick: () => {}
   }, {
-    label: 'GitHub',
-    icon: 'i-simple-icons-github',
+    label: 'Apple',
+    icon: 'i-simple-icons-apple',
     onClick: () => {}
   }]
   
 
 
-export type LoginFormSchema = z.output<typeof loginFormSchema>
