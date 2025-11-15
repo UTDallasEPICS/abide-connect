17e630e52ef3d0eebfdf1969dddfd1d011998444
Vikas Thoutam <vikas.thoutam@gmail.com>
2025-11-14 19:41:32 -0600

    chore: update package json and prisma config for merge

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,5 +8,6 @@ export default {
   schema: path.join("prisma", "schema"),
   migrations: {
     path: path.join("prisma", "migrations"),
+    seed: path.join("prisma", "seed.ts")
   }
 } satisfies PrismaConfig;
