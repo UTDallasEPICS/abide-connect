08acded0f516af97fe524f3e9a5102f74e1d32ae
Leila C. Igwegbe <leilaigw16@gmail.com>
2025-11-20 15:58:57 -0600

    Changes to naming of files

--- app/components/bottomNav.vue
+++ app/components/bottomNav.vue
@@ -37,7 +37,7 @@ const nav = ref([
        :key="item.id"
        class="flex flex-col items-center justify-center w-full"
      >
-       <!-- your existing button, unchanged -->
+       
        <UButton
          :icon="item.icon"
          :to="item.to"
