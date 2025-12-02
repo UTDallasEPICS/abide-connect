c10434c0ae0b0d8a4e3e346a20a1286a2e3196d8
Aagam Shah <shahaagam04@gmail.com>
2025-12-01 23:53:21 -0600

    feat: add session validation and redirect to login if user is not authenticated

--- server/api/auth/get-session.get.ts
+++ server/api/auth/get-session.get.ts
@@ -2,5 +2,8 @@ import { auth } from "~~/server/utils/auth";
 
 export default defineEventHandler(async (event) => {
     const session = await auth.api.getSession({ headers: event.headers });
+    if (!session?.user) {
+        await sendRedirect(event, '/auth/login', 302);
+    }
     return session;
 });
