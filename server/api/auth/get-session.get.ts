6cbfeed105a488479ea58942f4834475d5d2ff42
Aagam Shah <shahaagam04@gmail.com>
2025-12-02 01:45:36 -0600

    refactor: remove user authentication check and redirect in session retrieval handler

--- server/api/auth/get-session.get.ts
+++ server/api/auth/get-session.get.ts
@@ -2,8 +2,5 @@ import { auth } from "~~/server/utils/auth";
 
 export default defineEventHandler(async (event) => {
     const session = await auth.api.getSession({ headers: event.headers });
-    if (!session?.user) {
-        await sendRedirect(event, '/auth/login', 302);
-    }
     return session;
 });
