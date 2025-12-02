e37f3e839d4d38d53fea9bb8a47c0218523ec1c6
Aagam Shah <shahaagam04@gmail.com>
2025-12-01 23:51:45 -0600

    feat: implement relativeFetch utility and update auth middleware to use it

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,9 +1,12 @@
 import { authClient } from '~~/server/utils/auth-client';
+import { relativeFetch } from '@/lib/relativeFetch';
+
+
 
 export default defineNuxtRouteMiddleware(async (to, _from) => {
-	const { data } = await authClient.useSession(useFetch); 
-	if (to.path.startsWith("/volunteer")) {
-		if (!data) {
+	const { data: session } = await authClient.useSession(relativeFetch); 
+	if (!session?.value?.session) {
+		if (to.path.startsWith("/volunteer")) {
 			return navigateTo("/auth/login");
 		}
 	}
